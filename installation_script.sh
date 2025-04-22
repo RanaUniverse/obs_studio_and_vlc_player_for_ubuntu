0cb33c99886894a0a127e8f6759969304f818a8f
RanaUniverse <RanaUniverse321@gmail.com>
2025-04-22 07:39:09 +0530

    obs studio and vlc player installation

---
+++ installation_script.sh
@@ -0,0 +1,4 @@
+echo "OBS STUDIO & VLC PLAYER is installing..."
+sudo dpkg -i dependencies/*.deb
+echo "OBS STUDIO & VLC PLAYER has been installed please check this"
+echo "Run "obs" & "vlc" to run the apps using terminal"
